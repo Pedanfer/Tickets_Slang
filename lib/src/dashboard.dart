d7f474954df490e906dd78890b0abce8b664bcee
Pedanfer <ajfpedauye@protonmail.com>
2022-03-23 11:07:47 +0100

    El guardado en memoria persistente ya funciona

--- lib/src/dashboard.dart
+++ lib/src/dashboard.dart
@@ -4,7 +4,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final imgPicker = ImagePicker();
-var prefs;
+SharedPreferences? prefs;
 
 class DashBoard extends StatefulWidget {
   @override
@@ -18,44 +18,89 @@ class _DashBoardState extends State<DashBoard> {
 
   @override
   Widget build(BuildContext context) {
-    getPrefs();
     final dimension = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color.fromARGB(255, 178, 204, 226),
-              Color.fromARGB(255, 112, 221, 145),
-            ],
-          )),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              img,
-              Container(
+      body: FutureBuilder(
+        future: getPrefs(),
+        builder: (context, snapshot) {
+          return Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 178, 204, 226),
+                Color.fromARGB(255, 112, 221, 145),
+              ],
+            )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                img,
+                Container(
                   margin: EdgeInsets.symmetric(horizontal: 10),
                   child: Column(children: [
                     Container(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                          Container(
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                                margin: EdgeInsets.symmetric(vertical: 2.5),
+                                padding: EdgeInsets.symmetric(horizontal: 12),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    border: Border.all(color: Colors.black)),
+                                child: DropdownButton(
+                                    iconSize: 40,
+                                    icon: Icon(Icons.arrow_drop_down,
+                                        color: Colors.black),
+                                    items: prefs!
+                                        .getStringList('categList1')!
+                                        .map((String e) {
+                                      return DropdownMenuItem(
+                                          value: e, child: Text(e));
+                                    }).toList(),
+                                    onChanged: (value) => {
+                                          setState(() {
+                                            vista = value.toString();
+                                          })
+                                        },
+                                    hint: Text(
+                                      vista,
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          color: Color.fromARGB(
+                                              255, 74, 168, 245)),
+                                    ))),
+                            Container(
+                              child: IconButton(
+                                icon: Icon(Icons.add_link_sharp),
+                                onPressed: () {
+                                  photoFromCamera()
+                                      .then((value) => setState(() {
+                                            img = value;
+                                          }));
+                                },
+                              ),
+                            )
+                          ]),
+                    ),
+                    Container(
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
                               margin: EdgeInsets.symmetric(vertical: 2.5),
                               padding: EdgeInsets.symmetric(horizontal: 12),
                               decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(100),
-                                  border: Border.all(color: Colors.black)),
+                                borderRadius: BorderRadius.circular(100),
+                                border: Border.all(color: Colors.black),
+                              ),
                               child: DropdownButton(
-                                  iconSize: 40,
-                                  icon: Icon(Icons.arrow_drop_down,
-                                      color: Colors.black),
-                                  items: prefs
-                                      .getStringList('categList1')
+                                  items: prefs!
+                                      .getStringList('categList2')!
                                       .map((String e) {
                                     return DropdownMenuItem(
                                         value: e, child: Text(e));
@@ -65,76 +110,39 @@ class _DashBoardState extends State<DashBoard> {
                                           vista = value.toString();
                                         })
                                       },
-                                  hint: Text(
-                                    vista,
-                                    style: TextStyle(
-                                        fontSize: 15,
-                                        color:
-                                            Color.fromARGB(255, 74, 168, 245)),
-                                  ))),
-                          Container(
-                            child: IconButton(
-                              icon: Icon(Icons.add_link_sharp),
-                              onPressed: () {
-                                photoFromCamera().then((value) => setState(() {
-                                      img = value;
-                                    }));
-                              },
+                                  hint: Text(vista)),
                             ),
-                          )
-                        ])),
-                    Container(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 2.5),
-                            padding: EdgeInsets.symmetric(horizontal: 12),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              border: Border.all(color: Colors.black),
-                            ),
-                            child: DropdownButton(
-                                items: prefs
-                                    .getStringList('categList2')
-                                    .map((String e) {
-                                  return DropdownMenuItem(
-                                      value: e, child: Text(e));
-                                }).toList(),
-                                onChanged: (value) => {
-                                      setState(() {
-                                        vista = value.toString();
-                                      })
-                                    },
-                                hint: Text(vista)),
-                          ),
-                          Container(
-                            child: IconButton(
-                              icon: Icon(Icons.add_a_photo_sharp),
-                              onPressed: () {
-                                photoFromCamera().then((value) => setState(() {
-                                      img = value;
-                                    }));
-                              },
-                            ),
-                          )
-                        ]))
-                  ])),
+                            Container(
+                              child: IconButton(
+                                icon: Icon(Icons.add_a_photo_sharp),
+                                onPressed: () {
+                                  photoFromCamera()
+                                      .then((value) => setState(() {
+                                            img = value;
+                                          }));
+                                },
+                              ),
+                            )
+                          ]),
+                    )
+                  ]),
+                ),
 
-              Container(
+                Container(
                   margin: EdgeInsets.symmetric(vertical: 10),
                   height: dimension.height * 0.08,
                   width: dimension.width * 0.32,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                          Color.fromARGB(220, 161, 3, 64),
-                          Color.fromARGB(255, 238, 234, 7),
-                        ],
-                      )),
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Color.fromARGB(220, 161, 3, 64),
+                        Color.fromARGB(255, 238, 234, 7),
+                      ],
+                    ),
+                  ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -156,9 +164,13 @@ class _DashBoardState extends State<DashBoard> {
                         },
                       )
                     ],
-                  )) //))
-            ],
-          )),
+                  ),
+                ) //))
+              ],
+            ),
+          );
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color.fromARGB(255, 179, 185, 178),
         onTap: (index) {
@@ -179,31 +191,28 @@ class _DashBoardState extends State<DashBoard> {
 }
 
 Future<Image> photoFromCamera() async {
-  XFile? _pickedFile = await imgPicker.pickImage(source: ImageSource.camera);
+  var _pickedFile = await imgPicker.pickImage(source: ImageSource.camera);
   return Image.file(File(_pickedFile!.path), height: 500, width: 380);
 }
 
 Future<Image> photoFromGallery() async {
-  XFile? _pickedFile = await imgPicker.pickImage(source: ImageSource.gallery);
+  var _pickedFile = await imgPicker.pickImage(source: ImageSource.gallery);
   return Image.file(File(_pickedFile!.path));
 }
 
-void getPrefs() async {
-  await SharedPreferences.getInstance().then((value) => {
-        prefs = value,
-        print(prefs),
-        if (prefs.getBool('categsLoaded'))
-          {
-            prefs.setStringList('categList1', ['Añadir categoría']),
-            prefs.setStringList('categList2', ['Añadir categoría']),
-            prefs.setBool('categsLoaded', true)
-          }
-      });
+Future<SharedPreferences?> getPrefs() async {
+  //Esta función rula
+  prefs = await SharedPreferences.getInstance();
+  var catsLoaded = prefs!.getBool('categsLoaded') ?? false;
+  if (!catsLoaded) {
+    await prefs!.setStringList('categList1', ['Añadir categoría']);
+    await prefs!.setStringList('categList2', ['Añadir categoría']);
+    await prefs!.setBool('categsLoaded', true);
+  }
+  return prefs;
 }
 
 void saveCategToPrefs({required String categ, required int num}) {
   var nomLista = num == 1 ? 'listaCategs1' : 'listaCategs2';
-  getPrefs();
-  var listCategs = prefs?.getStringList(nomLista);
-  listCategs!.add(categ);
+  prefs!.getStringList(nomLista)!.add(categ);
 }
